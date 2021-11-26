b0552731541ba22d0fdf145c3801e9e24290f5dd
Alex Tello <alextello1@hotmail.com>
2021-11-26 16:06:52 -0600

    chore

--- container/src/App.js
+++ container/src/App.js
@@ -2,6 +2,11 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 
 export default () => {
-  return <MarketingApp />;
+  return (
+    <div>
+      <h1>HI THERE!</h1>
+      <MarketingApp />
+    </div>
+  );
   //test commit 6 correccion yml
 };
