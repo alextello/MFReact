1c29b4ee59be5906681d9842e4fc62c6c3f0a761
alextello <alextello1@hotmail.com>
2022-02-09 09:54:49 -0600

    feat: comunicacion de microfronends al contenedor en navegacion de rutas

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,16 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createMemoryHistory } from "history";
 import App from "./App";
+
 // Montar funcion para iniciar la app
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate }) => {
+  const history = createMemoryHistory();
+  if (onNavigate) history.listen(onNavigate);
+  ReactDOM.render(<App history={history} />, el);
 };
 
 // si estamos en dev y en aislamiento, llamar inmediatamente
 if (process.env.NODE_ENV === "development") {
   const devRoot = document.querySelector("#_marketing-dev-root");
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, {});
   }
 }
 
