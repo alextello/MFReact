9c5f1401c9978e1aa78a1e07efaded8463cc805f
alextello <alextello1@hotmail.com>
2022-02-10 08:03:38 -0600

    chore: agregando archivos

---
+++ auth/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Switch, Route, Router } from "react-router-dom";
+import { StylesProvider } from "@material-ui/core/styles";
+import { createGenerateClassName } from "@material-ui/core";
+import SignIn from "./components/Signin";
+import SignUp from "./components/Signup";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "au",
+});
+
+export default ({ history }) => {
+  return (
+    <div>
+      <StylesProvider>
+        <Router history={history}>
+          <Switch>
+            <Route path="/auth/signin" component={SignIn} />
+            <Route path="/auth/signup" component={SignUp} />
+          </Switch>
+        </Router>
+      </StylesProvider>
+    </div>
+  );
+};
