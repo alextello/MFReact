23d76b414bd0b335133376fcaa3188025cb3b30f
alextello <alextello1@hotmail.com>
2022-02-13 17:45:25 -0600

    resolviendo conflictos merge

--- container/src/App.js
+++ container/src/App.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import React, { useState } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import {
   StylesProvider,
@@ -15,6 +14,7 @@ const generateClassName = createGenerateClassName({
 });
 
 export default () => {
+  const [isSignedIn, setIsSignedIn] = useState(false);
   return (
     <BrowserRouter>
       <StylesProvider generateClassName={generateClassName}>
