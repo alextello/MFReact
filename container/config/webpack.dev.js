ac4bcffba5a084cf7d7d23e1d2371854bdcacafc
alextello <alextello1@hotmail.com>
2022-02-14 10:23:45 -0600

    Proyecto dashbaord

--- container/config/webpack.dev.js
+++ container/config/webpack.dev.js
@@ -1,25 +1,27 @@
-const { merge } = require("webpack-merge");
+const {merge} = require('webpack-merge');
 
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-const commonConfig = require("./webpack.common");
-const packageJson = require("../package.json");
+const ModuleFederationPlugin = require(
+    'webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
 const devConfig = {
-  mode: "development",
+  mode: 'development',
   output: {
-    publicPath: "http://localhost:7080/",
+    publicPath: 'http://localhost:7080/',
   },
   devServer: {
     port: 7080,
     historyApiFallback: {
-      index: "index.html",
+      index: 'index.html',
     },
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: "container",
+      name: 'container',
       remotes: {
-        marketing: "marketing@http://localhost:8081/remoteEntry.js",
-        auth: "auth@http://localhost:8082/remoteEntry.js",
+        marketing: 'marketing@http://localhost:8081/remoteEntry.js',
+        auth: 'auth@http://localhost:8082/remoteEntry.js',
+        dashboard: 'dashboard@http://localhost:8083/remoteEntry.js',
       },
       shared: packageJson.dependencies,
     }),
--- container/src/App.js
+++ container/src/App.js
@@ -7,8 +7,10 @@ import {
 
 import Header from "./components/Header";
 import Progress from "./components/Progress";
+
 const MarketingLazy = lazy(() => import("./components/MarketingApp"));
 const AuthLazy = lazy(() => import("./components/AuthApp"));
+const DashboardLazy = lazy(() => import("./components/DashboardApp"));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: "co",
@@ -29,6 +31,7 @@ export default () => {
               <Route path="/auth">
                 <AuthLazy onSignIn={() => setIsSignedIn(true)} />
               </Route>
+              <Route path="/dashboard" component={DashboardLazy} />
               <Route path="/" component={MarketingLazy} />
             </Switch>
           </Suspense>
